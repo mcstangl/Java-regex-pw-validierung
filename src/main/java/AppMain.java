b8b15b30b3e13b95d738f64aee1c78fc8a1366ea
Matthias Stangl <fazmaz@gmx.de>
2021-07-17 18:40:45 +0200

    added commandline interface

--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class AppMain {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isValidPassword = false;
+
+        while (!isValidPassword) {
+
+            System.out.print("Bitte geben Sie ein Passwort ein: ");
+            String password = scanner.nextLine();
+
+
+            if (Password.validate(password)) {
+                System.out.println("Ihr Passwort ist gültig");
+                isValidPassword = true;
+            }
+            else {
+                System.out.println("Passwort ist nicht gültig!");
+            }
+
+        }
+
 
     }
 }
